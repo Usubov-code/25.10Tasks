0687be7dfeb30096dd826c0d2f61080105f81032
Usubov-code <tu201806104@code.edu.az>
2021-10-25 18:10:40 +0400

    Add project files.

---
+++ 25.10Tasks/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace _25._10Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //        //////1-dən 100-ə qədər ədədləri console-da çap edin;\\\\\\\\\\\\
+            //        int a = 100;
+            //        for (int i = 1; i <= a; i++)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+
+
+
+            ////////////1_dən_100_ə_qədər_tək_ədədlər\\\\\\\\\
+            //int a = 100;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i + " tek eded ");
+
+            //    }
+            //}
+
+
+            /////User tərəfindən daxil edilmiş ədədin 2 mislini(2-yə vurmaq) console-da çap edin;\\\\\\\\\
+            //Console.WriteLine("enter number a;");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(a * 2);
+
+
+            ///  task 1 write_10_natural_numbers\\\\\\\\\\\ 
+
+            //int a = 10;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+
+            ////taks 2\\\\\
+            //int sum=0,a = 10;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    sum = sum + i;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+            ////task 3\\\\
+
+            //double pi = Math.PI, r = 12;
+
+            //var l = 2 * pi * r;
+            //var s = pi * r * r;
+
+            //Console.WriteLine(l + " perimetr");
+            //Console.WriteLine(s + " area circle");
+
+
+
+
+
+            //////tast 4\\\\\
+
+            //double a = 20, b=12;
+
+            //Console.WriteLine(a*b + " multiplication");
+            //Console.WriteLine(a-b + " minus");
+            //Console.WriteLine(a+b + " plus");
+            //Console.WriteLine(a/b + " division");
+
+
+            ////TASK 5 \\\\\\\\\\\\\\\\\\
+
+            //string  a =Console.ReadLine();
+            //int b = Int32.Parse(Console.ReadLine());
+
+            //Console.Clear();
+
+            //for (int i = 0, c = b; i < b; i++)
+            //{
+
+            //    for (int j = 0; j <c; j++)
+            //    {
+            //        Console.Write(a);
+
+            //    }
+            //    Console.WriteLine();
+            //    c--;
+            //}
+
+
+
+
+
+
+
+
+            ///// task 6 \\\\\\\\\\
+
+
+            //string usernameDatabase = "elsad";
+            //string passwordDatabase = "12345";
+
+            //int chance = 3;
+
+            //while (true)
+            //{
+            //    Console.Write("Enter username: ");
+            //    string username = Console.ReadLine();
+            //    Console.Write("Enter password: ");
+            //    string password = Console.ReadLine();
+
+            //    if (usernameDatabase != username || passwordDatabase != password)
+            //    {
+            //        if (chance > 1)
+            //        {
+            //            chance--;
+            //            Console.WriteLine("Wrong passowrd or username! Left chance: " + chance);
+            //            continue;
+            //        }
+
+            //        Console.WriteLine("You entered 3 wrong attempts!");
+            //        break;
+            //    }
+
+            //    Console.WriteLine("Successfully login, " + username + "!");
+            //    break;
+            //}
+
+            
+
+
+
+
+
+        }
+    }
+}
